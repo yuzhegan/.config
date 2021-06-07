abc1fada1507fd92daa6da9c42f75a4797780caa
yuzhegan <boflyyang@163.com>
2021-06-07 10:03:42 +0800

    change some settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
-function time --description="Bash time function"
-  command time --portability $argv
+function time_som --description="Bash time_som function"
+  command time_som --portability $argv
 end
 
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
-
