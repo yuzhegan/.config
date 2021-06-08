86d4cbaba21d27c49b07f5bb7d89c66f84a11613
yuzhegan <boflyyang@163.com>
2021-06-08 09:10:15 +0800

    lazygit <c-u><c-e> to <c-k><c-j> fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
-function time --description="Bash time function"
-  command time --portability $argv
+function time_awm --description="Bash time_awm function"
+  command time_awm --portability $argv
 end
 
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
