794facff4cca948ba237d714646701e567c7c3f9
yuzhe <boflyyang@163.com>
2021-06-08 18:32:49 +0800

    termrial <c-t> to open fzf

---
+++ fish/functions/`.fish
@@ -0,0 +1,5 @@
+# Defined via `source`
+function ` --wraps=fzf\\\ fzf\\\ --preview\\\ \\\'head\\\ -100\\\ \\\{\\\}\\\'\\n\ncommand\ fzf\ --preview\ \'head\ -100\ \{\}\'\ \  --description alias\ `\ fzf\\\ fzf\\\ --preview\\\ \\\'head\\\ -100\\\ \\\{\\\}\\\'\\n\ncommand\ fzf\ --preview\ \'head\ -100\ \{\}\'\ \ 
+  fzf\ fzf\ --preview\ \'head\ -100\ \{\}\'\n
+command fzf --preview 'head -100 {}'   $argv; 
+end
