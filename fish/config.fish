4ee344dafbc98feb1efd4ee8a40e41fc9b5d9b3f
yuzhegan <boflyyang@163.com>
2021-06-12 20:59:05 +0800

    fish gcp gro gpl xo.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 function time_awm --description="Bash time_awm function"
   command time_awm --portability $argv
 end
+
+
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
 set fish_greeting ""
 export EDITOR=/usr/bin/nvim
 export VISUAL=/usr/bin/nvim
+
--- fish/functions/vim.fish
+++ fish/functions/vim.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 1
-function vim --description 'alias vim nvim'
-	nvim  $argv;
+# Defined via `source`
+function vim --wraps=nvim --description 'alias vim nvim'
+  nvim $argv; 
 end
